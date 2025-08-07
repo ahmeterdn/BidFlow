f310e016d137daeffd49200aed72bb81da91cac0
ahmeterdn <ahmeterdnn1@gmail.com>
2025-08-07 13:24:16 +0300

    updated program.cs

--- MyApp.Api/Program.cs
+++ MyApp.Api/Program.cs
@@ -1,11 +1,13 @@
 using System.Text;
-using AutoMapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
-using BidFlow.Api.Data;
+using BidFlow.Data;
 using BidFlow.Api.Mappings;
+using BidFlow.Repositories;
+using BidFlow.Services;
+using FluentValidation;
+using BidFlow.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +28,16 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<AuditInterceptor>();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
