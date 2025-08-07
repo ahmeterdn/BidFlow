292b5a3db9ace4d09770a81c8203ae3227b7d4a7
ahmeterdn <ahmeterdnn1@gmail.com>
2025-08-07 13:16:33 +0300

    Validation extension

---
+++ MyApp.Api/Common/ValidationExtensions.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+
+namespace BidFlow.Common
+{
+    public static class ValidationExtensions
+    {
+        public static async Task<Result<T>> ValidateAsync<T>(this IValidator<T> validator, T instance)
+        {
+            var validationResult = await validator.ValidateAsync(instance);
+
+            if (validationResult.IsValid)
+            {
+                return Result<T>.Success(instance);
+            }
+
+            var errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+            return Result<T>.Failure(ErrorMessages.ValidationFailed, errors);
+        }
+
+        public static Result<T> Validate<T>(this IValidator<T> validator, T instance)
+        {
+            var validationResult = validator.Validate(instance);
+
+            if (validationResult.IsValid)
+            {
+                return Result<T>.Success(instance);
+            }
+
+            var errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+            return Result<T>.Failure(ErrorMessages.ValidationFailed, errors);
+        }
+    }
+}
