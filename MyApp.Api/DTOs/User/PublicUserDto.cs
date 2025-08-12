b1fabaf3276717dac1b812bf70cbaaad382be9ba
ahmeterdn <ahmeterdnn1@gmail.com>
2025-08-12 13:00:05 +0300

    Applied additional fixes

---
+++ MyApp.Api/DTOs/User/PublicUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BidFlow.DTOs.User
+{
+    public class PublicUserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
